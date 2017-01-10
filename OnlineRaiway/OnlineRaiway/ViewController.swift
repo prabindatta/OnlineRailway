069b06f021439a87a445ef790bf59c48faea12a7
Prabin K Datta <prabindatta@gmail.com>
2017-01-10 23:45:04 +0530

    Setup Project

---
+++ OnlineRaiway/OnlineRaiway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnlineRaiway
+//
+//  Created by Prabin K Datta on 10/01/17.
+//  Copyright © 2017 Prabin K Datta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
